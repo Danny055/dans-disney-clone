39bf896159e5737222fdb7e2913a132921a369cd
Daniel Rodriguez <dannyrodz055@gmail.com>
2021-09-24 00:14:58 -0400

    Adding movie Images from Firestore DB functionality

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import  userReducer from "../features/user/userSlice";
-
+import  movieReducer from "../features/movie/movieSlice";
 const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        movie: movieReducer,
     },
     middleware: getDefaultMiddleware({
         serializableCheck: false,
