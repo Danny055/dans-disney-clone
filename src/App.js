dc7412faa783a616f1857163d0ed14c07eb6e71a
Daniel Rodriguez <dannyrodz055@gmail.com>
2021-09-29 23:59:19 -0400

    Adding Details Page with firebase db functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Login from './components/Login';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
         </Route>
         <Route path="/home">
           <Home />
+        </Route>
+        <Route path="/detail/:id">
+          <Detail />
         </Route>
       </Switch>
       </Router>
